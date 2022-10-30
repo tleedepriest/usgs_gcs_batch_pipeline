423e5dd4ac22510aa0c4682ee0d2e088eeaa1f0d
Travis <tleedepriest@gmail.com>
2022-10-30 09:33:13 -0400

    updated the variables for the name of the data lake and the data wharehouse. Set the variable for project id.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 locals {
-  data_lake_bucket = "dtc_data_lake"
+  data_lake_bucket = "usgs_data_lake"
 }
 
 variable "project" {
-  description = "Your GCP Project ID"
+  description = "usgs_equake"
 }
 
 variable "region" {
@@ -20,5 +20,5 @@ variable "storage_class" {
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
   type = string
-  default = "trips_data_all"
+  default = "equake_data"
 }
